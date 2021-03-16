1a04902ed720403745304d9e72ce7324afd6e4a3
Nickchen1125 <chunchih2021@gmail.com>
2021-03-16 11:27:16 +0800

    update

--- products.py
+++ products.py
@@ -1,4 +1,19 @@
+import os
 products = []
+
+if os.path.isfile('products.csv'):
+	print('已找到檔案')
+
+	with open('products.csv', 'r', encoding='UTF-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name , price = line.strip().split(',')
+			products.append([name,price])
+	print(products)
+else:
+	print('找不到檔案')
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -15,3 +30,4 @@ with open('products.csv', 'w', encoding='UTF-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
+
